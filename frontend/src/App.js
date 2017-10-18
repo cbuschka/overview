318086c445661bccbe131afa8a567c30db0094a1
conni <c.buschka@arcusx.com>
2017-10-18 08:14:45 +0200

    Added a webapi/frontend skeleton.

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import './App.css';
+import {Route, BrowserRouter, Redirect, Switch} from 'react-router-dom';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+    }
+
+    componentWillUnmount() {
+    }
+
+    render() {
+        return (
+            <div className="App">theapp</div>
+        );
+    }
+}
+
+export default App;
---
+++ webapi/src/routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var model = { title: 'index' };
+  res.render('index', model);
+});
+
+module.exports = router;
